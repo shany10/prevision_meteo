1613386ebd40be1b71ad4e427e708be3f3cc71e4
stage <stage@digiplace.fr>
2024-06-27 18:36:23 +0200

    Ajout de deux fonctionalités de test

--- backend/app/Http/Controllers/TemperatureController.php
+++ backend/app/Http/Controllers/TemperatureController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Http\JsonResponse;
@@ -9,58 +10,104 @@ use Illuminate\Support\Facades\DB;
 
 class TemperatureController extends Controller
 {
-
     public function get_outfit(Request $request): JsonResponse
     {
         $request_data = $request->all();
-    
-        $response = Http::get("http://api.weatherapi.com/v1/current.json?key=" .
-            env('WEATHER_API_KEY') .
-            "&q=" . $request_data["weather"]["city"]);
+        try {
+            $response = Http::get("http://api.weatherapi.com/v1/current.json?key=" .
+                env('WEATHER_API_KEY') .
+                "&q=" . $request_data["weather"]["city"]);
+        } catch (Exception $e) {
+            echo "Problème survenu lors de l'appelle de l'api Weather : $e";
+            die;
+        }
 
         $data = $response->json();
         $temperature = $this->get_temperature($data);
         $weather = $this->compare_temperature($temperature);
         $outfit = $this->find_outfit($weather);
-        $final_respons = $this->data_constructor($request_data["weather"]["city"], $weather, $outfit);
-        return response()->json($final_respons);
+        $final_response = $this->data_constructor($request_data["weather"]["city"], $weather, $outfit);
+        return response()->json($final_response);
     }
 
+
+    //TEST
+
+    //exemple => http://localhost:8000/api/test_temperature/15  
+    public function test_temperature($temperature)
+    {
+        $weather = $this->compare_temperature($temperature);
+        var_dump($weather);
+    }
+
+    //exemple => http://localhost:8000/api/test_outfit/hot 
+    public function test_outfit($weather)
+    {
+        $outfit = $this->find_outfit($weather);
+        var_dump($outfit);
+    }
 
     //METHOD
 
     private function get_temperature($data)
     {
-        if (!empty($data["error"])) return "ERROR => cuntry not found";
-        return $data["current"]["temp_c"];
+        if (!empty($data["current"])) return $data["current"]["temp_c"];
+        else if (!empty($data["error"])) {
+            var_dump($data["error"]);
+            die;
+        } else {
+            var_dump("ERROR => Erreur indéterminée");
+            die;
+        }
     }
 
     private function compare_temperature($temperature)
     {
+        preg_match('/[a-z A-Z]/', $temperature, $match);
+        if (!empty($match)) {
+            var_dump("ERROR => Format de temperature invalide");
+            die;
+        }
         if ($temperature < 10) return "cold";
         elseif ($temperature >= 10 && $temperature < 20) return "lukewarm";
         elseif ($temperature >= 20) return "hot";
     }
 
-    private function find_outfit($weather)
+    private function find_outfit(string $weather)
     {
+        if ($weather != "cold" && $weather != "lukewarm" && $weather != "hot") {
+            var_dump("ERROR => categorie inconnu");
+            die;
+        }
+
         return DB::table("outfits")
             ->where("categorie", $weather)
             ->select(["id", "name", "categorie"])
             ->get();
     }
 
-    private function data_constructor($city, $weather, $outfit)
+    private function data_constructor(string $city, string $weather, $outfit)
     {
-        $arr_data = [
-            "products" => $outfit,
-            "weather" => [
-                "city" => $city,
-                "waether" => $weather,
-                "date" => ""
-            ]
-        ];
+        if ($weather != "hot" && $weather != "cold" && $weather != "lukewarm") {
+            var_dump("ERROR => categorie inconnu");
+            die;
+        }
+        if (is_array($outfit)) {
+            if (count($outfit) > 0) {
+                $arr_data = [
+                    "products" => $outfit,
+                    "weather" => [
+                        "city" => $city,
+                        "waether" => $weather,
+                        "date" => ""
+                    ]
+                ];
 
-        return $arr_data;
+                return $arr_data;
+            }
+        } else {
+            var_dump("Navré il n'y a plus d'article en stock");
+            die;
+        }
     }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -19,3 +19,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('/getOutfit', [TemperatureController::class, 'get_outfit']);
+
+//Test
+Route::get('/test_temperature/{temperature}', [TemperatureController::class, 'test_temperature']);
+Route::get('/test_outfit/{weather}', [TemperatureController::class, 'test_outfit']);
+
